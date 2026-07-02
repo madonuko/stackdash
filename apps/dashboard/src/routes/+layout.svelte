b2b8ade99b3590d8805d5ed8946e96e3bf51d574
Willow Reed <cypress@fyralabs.com>
2026-07-02 17:02:58 -0600

    add static routes for png and ico files for webkit favicon

--- apps/dashboard/src/routes/+layout.svelte
+++ apps/dashboard/src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import NavigationProgress from '$lib/components/navigation-progress.svelte';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <NavigationProgress />
 
